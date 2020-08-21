a5daaeb3ac7e85c2892e659a6f1874686d33a60b
unknown <dhivakar0227@gmail.com>
2020-08-21 20:39:34 +0000

    Fifth commit

--- src/Clients/client.go
+++ src/Clients/client.go
@@ -24,7 +24,7 @@ func main() {
 		Biller: &billingpb.Bill{
 			FirstName:   "Dhivakar",
 			LastName:    "Jeganathan",
-			InvoiceDate: "8/21/2021",
+			InvoiceDate: "8/21/2022",
 			InvoiceAmt:  2000,
 		},
 	}
